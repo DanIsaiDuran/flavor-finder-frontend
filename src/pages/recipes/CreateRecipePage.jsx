c521bdbdb309aba23497f178d1a27d6542e5ceeb
DanIsaiDuran <dan.duran5635@alumnos.udg.mx>
2024-11-08 17:39:40 -0600

    feat: implemented logic to use same page to create and edit a recipe

--- src/pages/recipes/CreateRecipePage.jsx
+++ src/pages/recipes/CreateRecipePage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const CreateRecipePage = () => {
   
@@ -14,11 +14,23 @@ const CreateRecipePage = () => {
     status: 'ACTIVE',
     tools: ''
   });
-  let navigate = useNavigate();
 
-  const routeChange = () => {
-    navigate('/');
-  };
+  const { recipeId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(recipeId) {
+      axios.get(`http://localhost:8080/api/v1/recipe/${recipeId}` , {
+        validateStatus: (status) => status >= 200 && status <= 302,
+      })
+      .then(response => {
+        setRecipe(response.data);
+      })
+      .catch(error => {
+        console.error('Error al cargar la receta', error);
+      });
+    }
+  }, [recipeId]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -28,39 +40,44 @@ const CreateRecipePage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8080/api/v1/recipe/1', recipe, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+      const url = recipeId ? `http://localhost:8080/api/v1/recipe/update/${recipeId}` : 'http://localhost:8080/api/v1/recipe/1';
+      const method = recipeId ? 'put' : 'post';
+
+      const response = await axios({
+        method: method,
+        url: url,
+        data: recipe,
+        headers: { 'Content-Type': 'application/json' }
       });
 
-      if (response.status === 201) {
+      if (response.status === 201 || response.status === 200) {
         console.log('Datos enviados exitosamente');
+        navigate('/');
       } else {
         console.error('Error al enviar los datos');
       }
 
     } catch (error) {
-      console.error('Error en la solicitud:', error.response?.data);
+      console.error('Error en la solicitud:', error);
     }
   };
 
   return (
     <>
-      <div className='max-w-sm sm:max-w-7xl mx-auto p-4 bg-secondary rounded-3xl'>
+      <div className='max-w-sm mx-6 md:mx-12 sm:max-w-5xl lg:max-w-full xl:mx-12 p-4 bg-secondary rounded-3xl'>
       <form className="w-full" onSubmit={handleSubmit}>
         <div className="flex flex-wrap mb-3">
-          <div className="w-full sm:w-1/2 px-3 mb-6 md:mb-0">
+          <div className="w-full sm:w-1/2 px-3 mb-3 md:mb-0">
             <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="name">
               Nombre
             </label>
-            <input id="name" name='name' type="text" placeholder="Nombre" value={recipe.name} onChange={handleChange} className=" block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  />
+            <input id="name" name='name' type="text" placeholder="Nombre" value={recipe.name} onChange={handleChange} className=" block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white"  />
           </div>
           <div className="w-full sm:w-1/4 px-3">
             <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="difficulty">
               Dificultad
             </label>
-            <select value={recipe.difficulty} onChange={handleChange} id="difficulty" name='difficulty' className=" block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+            <select value={recipe.difficulty} onChange={handleChange} id="difficulty" name='difficulty' className="appearance-none block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-3">
               <option value="">Seleccione una opción</option>
               <option value='VERY_EASY'>Muy facil</option>
               <option value='EASY'>Facil</option>
@@ -97,13 +114,13 @@ const CreateRecipePage = () => {
             <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-city">
               Ingredientes
             </label>
-            <textarea name="ingredients" id="ingredients" value={recipe.ingredients} onChange={handleChange} className="appearance-none w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></textarea>
+            <textarea name="ingredients" id="ingredients" value={recipe.ingredients} onChange={handleChange} className="appearance-none w-full h-24 bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></textarea>
           </div>
           <div className="w-full sm:w-1/2 px-3 mb-6 md:mb-0">
             <label className="block uppercase tracking-wide text-white text-xs font-bold mb-2" htmlFor="grid-city">
               Herramientas
             </label>
-            <textarea name="tools" id="tools" value={recipe.tools} onChange={handleChange} className="appearance-none w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></textarea>
+            <textarea name="tools" id="tools" value={recipe.tools} onChange={handleChange} className="appearance-none w-full h-24 bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"></textarea>
           </div>
         </div>
         <div className='flex flex-row-reverse'>
@@ -111,7 +128,7 @@ const CreateRecipePage = () => {
             <button type='submit' className='bg-primary-900 rounded-full px-3 py-2 mx-3 text-white font-bold'>Enviar</button>
           </div>
           <div>
-            <button type='button' onClick={routeChange} className='bg-primary-900 rounded-full px-3 py-2 mx-3 text-white font-bold'>Cancelar</button>
+            <button type='button' onClick={() => navigate('/')} className='bg-primary-900 rounded-full px-3 py-2 mx-3 text-white font-bold'>Cancelar</button>
           </div>
         </div>
       </form>
