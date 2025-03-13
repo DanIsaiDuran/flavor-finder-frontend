360c551fd8a2ece713a8051be27e1334c06bad1c
Dan Duran <danisaidutru@gmail.com>
2025-03-12 21:45:33 -0600

    feat: login page implementation started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../assets/Logo1.png'
+import logosvg from '../assets/Icono Negro.svg'
 
 function Navbar() {
   return (
     <nav className='bg-primary-700 text-black mt-4 mb-8 mx-6 md:mx-12 py-3 rounded-full'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
             <div className='font-bold flex items-center space-x-1'>
-                <img src={logo} alt="Logo" className='w-8'/>
+                <img src={logosvg} alt="Logo" className='w-8'/>
                 <div className='flex flex-col leading-none'>
                     <span>Flavor</span>
                     <span>Finder</span>
