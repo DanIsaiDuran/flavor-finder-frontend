34f6682fe960eb3ef0d6bc989faab2d58963ace3
Dan Duran <danisaidutru@gmail.com>
2025-03-13 22:14:34 -0600

    feat: use of authcontext to access to logged user and login/logout implemented

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../components/context/AuthContext'
 
 function Recipes() {
+
+  const { user } = useContext(AuthContext);
+
   return (
     <>
         <h1>Hello world</h1>
+        {user ? (
+          <p>{user.username}</p>
+        ) : 
+        (
+          <p>asd</p>
+        )}
+        
     </>
   )
 }
