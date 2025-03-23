d298bc6cb2df726c919935641e2547554b2531d8
Dan Duran <danisaidutru@gmail.com>
2025-03-23 13:09:08 -0600

    feat: isAdmin variable added to authContext

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,17 +1,23 @@
 import { createContext, useState, useEffect } from "react";
 import API from "../../services/api";
 import { useNavigate } from "react-router-dom";
+import {jwtDecode} from "jwt-decode";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
+      const decoded = jwtDecode(JSON.parse(storedUser).jwt);
+      if (decoded?.authorities === "ROLE_ADMIN") {
+        setIsAdmin(true);
+      }
     }
     setLoading(false);
   }, []);
@@ -22,6 +28,10 @@ export const AuthProvider = ({ children }) => {
       const { data } = await API.post("/auth/login", credentials);
       localStorage.setItem("user", JSON.stringify(data)); // Store user data
       setUser(data);
+      const decoded = jwtDecode(user.jwt);
+      if (decoded?.authorities === "ROLE_ADMIN") {
+        setIsAdmin(true);
+      }
       return true;
     } catch (error) {
       console.error("Login failed:", error.response?.data || error.message);
@@ -35,6 +45,10 @@ export const AuthProvider = ({ children }) => {
       const { data } = await API.post("/auth/sign-up", credentials);
       localStorage.setItem("user", JSON.stringify(data)); // Store user data
       setUser(data);
+      const decoded = jwtDecode(user.jwt);
+      if (decoded?.authorities === "ROLE_ADMIN") {
+        setIsAdmin(true);
+      }
       return {success: true, error: null};
     } catch (error) {
       console.error("Registration failed:", error.response?.data || error.message);
@@ -49,7 +63,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, register, logout, loading }}>
+    <AuthContext.Provider value={{ user, login, register, logout, loading, isAdmin }}>
       {children}
     </AuthContext.Provider>
   );
