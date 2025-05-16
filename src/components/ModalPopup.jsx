2816f618e16bd845bf8b7ce8b3a082bd1d889ced
Dan Duran <danisaidutru@gmail.com>
2025-05-15 19:53:37 -0600

    feat: Icon added to modalPopup

--- src/components/ModalPopup.jsx
+++ src/components/ModalPopup.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import { Button, Modal, ModalBody, ModalHeader } from "flowbite-react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 
 const ModalPopup = ({openModal, setOpenModal, bodyMessage}) => {
     
@@ -9,8 +11,9 @@ const ModalPopup = ({openModal, setOpenModal, bodyMessage}) => {
             <Modal show={openModal} size="lg" position='top-center' onClose={() => setOpenModal(false)} popup>
                 <ModalHeader className='bg-secondary rounded-t-md'/>
                 <ModalBody className='bg-secondary  rounded-b-md'>
-                <div className="text-center m-5">
-                    <h3 className="mb-5 text-lg font-normal text-white dark:text-white">
+                <div className="text-center pb-4">
+                    <FontAwesomeIcon icon={faCircleExclamation} className='text-white text-3xl'/>
+                    <h3 className="mb-5 mt-2 text-lg font-normal text-white dark:text-white">
                         {bodyMessage}
                     </h3>
                     <div className="flex justify-center gap-4">
